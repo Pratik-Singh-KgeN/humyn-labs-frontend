82cf94cb37625fcd4edc3eefe91ad33b7214836e
Shivam Shukla <shivam@gkmit.co>
2026-01-22 14:04:20 +0530

    fix: remove className from template

--- templates/next-app/app/layout.tsx
+++ templates/next-app/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="humyn-bd">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
